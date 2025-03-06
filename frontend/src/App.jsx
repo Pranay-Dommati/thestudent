dddc2374929f597c2153aed442328e616c4d48c7
banny <bannydommati@gmail.com>
2025-03-06 15:21:22 +0530

    enhancing ui of chatbot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
+import ChatBotPage from './components/Chatbot/ChatbotPage';
+
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatBotPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
