971a4b5df75341478d5da950eef5d053d54a830f
banny <bannydommati@gmail.com>
2025-03-06 18:08:38 +0530

    adding coursedetails in it's page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import Courses from './components/Courses/Courses';
 import ChatBotPage from './components/Chatbot/ChatbotPage';
-import CourseDetailsPage from './components/Courses/CourseDetailsPage/CourseDetailsPage';
+import CourseDetailsPage from './components/CourseDetails/CourseDetailsPage/CourseDetailsPage';
 
 function App() {
   return (
--- frontend/src/components/CourseDetails/CourseDetailsPage/CourseDetailsPage.jsx
+++ frontend/src/components/CourseDetails/CourseDetailsPage/CourseDetailsPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import CourseDetails from '../../CourseDetails/CourseDetails';
+import CourseDetails from '../CourseDetails';
 import Navbar from '../../Navbar/Navbar';
 import Footer from '../../Footer/Footer';
 
