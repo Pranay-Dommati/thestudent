24d2b4fb426952bba6a79c7cd202fdcaad0ad988
banny <bannydommati@gmail.com>
2026-08-02 16:21:12 +0530

    before deleting the deprecated frontend folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const base = env.VITE_BASE_PATH || '/'
 
+  
   return {
   plugins: [react()],
   base,
